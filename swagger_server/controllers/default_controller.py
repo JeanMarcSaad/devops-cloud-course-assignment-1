1b0adfcad101c3f6b03a5bcc53a64d2cecb41d4f
Jean-Marc Saad <jeanmarcsd@outlook.com>
2022-02-11 15:03:29 +0100

    Setup Flask Server + Implemented Student Service

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -3,6 +3,7 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import *
 
 
 def add_student(body=None):  # noqa: E501
@@ -17,7 +18,8 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student_by_id(student_id):  # noqa: E501
@@ -30,7 +32,10 @@ def delete_student_by_id(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    if student_id:
+        return delete(student_id)
+
+    return 500, 'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +48,8 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+
+    if student_id:
+        return get_by_id(student_id)
+
+    return 500, 'error'
