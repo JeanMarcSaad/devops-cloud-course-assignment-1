c66e336e2a39e40806af4e93cb17cffde6f1dc7b
Jean-Marc Saad <jeanmarcsd@outlook.com>
2022-02-14 23:21:53 +0100

    Added MongoService Implementation

--- swagger_server/service/mongodb_service.py
+++ swagger_server/service/mongodb_service.py
@@ -2,56 +2,40 @@ import os
 import tempfile
 
 from pymongo import MongoClient
-import json
+from bson.objectid import ObjectId
 
-from swagger_server.models import Student
+# from swagger_server import util
+# from swagger_server.models import Student
+# import json
 
 client = MongoClient("mongodb://localhost:27017")
 db = client.local
-
 collection = db.students
 
-# Issue the serverStatus command and print the results
-# serverStatusResult=db.command("serverStatus")
-# pprint(serverStatusResult)
-
 def add(student=None):
-    # queries = []
-    # query = Query()
-    # queries.append(query.first_name == student.first_name)
-    # queries.append(query.last_name == student.last_name)
-    # query = reduce(lambda a, b: a & b, queries)
-    # res = student_db.search(query)
+    _student = collection.find_one({'student_id': student.student_id})
 
-    # if res:
-    #     return 'already exists', 409
+    if _student:
+        return 'already exists', 409
 
-    # doc_id = student_db.insert(student.to_dict())
-    # student.student_id = doc_id
+    student_dict = student.to_dict()
+    student_entry = collection.insert_one(student_dict)
 
-    _student = student
+    if student_entry.inserted_id:
+        return student.student_id
 
-    collection.insert_one(student.to_str())
-
-    __student = student.to_str()
-
-    # student_object = collection.insert_one(student.to_str())
-    # return student_object.inserted_id
-    return 0
+    return 'internal error', 500
 
 def get_by_id(student_id=None, subject=None):
-    # # student = student_db.get(doc_id=int(student_id))
-    # db.find({id()})
-    # if not student:
-    #     return 'not found', 404
-    # student['student_id'] = student_id
-    # return student
-    return None
+    _student = collection.find_one({'student_id': student_id})
+    if not _student:
+        return 'not found', 404
+    _student['_id'] = str(_student['_id'])
+    return _student
 
 def delete(student_id=None):
-    # student = student_db.get(doc_id=int(student_id))
-    # if not student:
-    #     return 'not found', 404
-    # student_db.remove(doc_ids=[int(student_id)])
-    # return student_id
-    return None
+    _student = collection.find_one({'student_id': student_id})
+    if not _student:
+        return 'not found', 404
+    collection.delete_one(({'student_id': student_id}))
+    return student_id
